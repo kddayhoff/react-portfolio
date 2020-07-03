fce6d8d1ad8456344d90258c45af5de3cd40a7aa
Karla Dayhoff <kddayhoff@gmail.com>
2020-07-03 00:14:14 -0500

    Updates

---
+++ precache-manifest.fc74f18a5573799a2eddb43a3610a875.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "934849fa3a0d813a0ada05119310e1c2",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "0b0c961f7990f0188e27",
+    "url": "/react-portfolio/static/css/main.a10cde79.chunk.css"
+  },
+  {
+    "revision": "4d447403fe12251d810c",
+    "url": "/react-portfolio/static/js/2.c44f15e3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-portfolio/static/js/2.c44f15e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b0c961f7990f0188e27",
+    "url": "/react-portfolio/static/js/main.9558597e.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "f72dcce4316bd2690e6616ef51d11808",
+    "url": "/react-portfolio/static/media/notetaker.f72dcce4.png"
+  },
+  {
+    "revision": "6d85023fb5b686e0dfbe99ae53da345e",
+    "url": "/react-portfolio/static/media/password-generator.6d85023f.png"
+  },
+  {
+    "revision": "2637c4765b83ee6b315fc09481cf958c",
+    "url": "/react-portfolio/static/media/person-directory.2637c476.png"
+  },
+  {
+    "revision": "a44a1359515629ec5195b3d78ac10e9a",
+    "url": "/react-portfolio/static/media/profilepicture.a44a1359.jpg"
+  },
+  {
+    "revision": "b1aa5fbb55edd1f3b99877de8e960cc1",
+    "url": "/react-portfolio/static/media/route-404.b1aa5fbb.png"
+  },
+  {
+    "revision": "baf70ffb4adba8dc7d9d29d19ca40cce",
+    "url": "/react-portfolio/static/media/space.baf70ffb.png"
+  },
+  {
+    "revision": "5b5e6dcdebdc83e55e845e4abc5dc46f",
+    "url": "/react-portfolio/static/media/weather-dashboard.5b5e6dcd.png"
+  }
+]);
